de7b34e46ec9309a54c2097c067796e5ddab40f5
gyes00205 <gyes00205@gmail.com>
2022-07-16 16:11:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,9 @@ python finetune.py --save_path results/finetune_anynet_refine \
                    --pretrained results/pretrained_anynet_refine/checkpoint.tar \
                    --with_refine
 
+python finetune.py --save_path results/finetune_anynet_refine_2012 \
+                   --pretrained results/pretrained_anynet_refine/checkpoint.tar \
+                   --with_refine \
+                   --datapath /media/bsplab/62948A5B948A3219/data_stereo_flow_2012/training/ \
+                   --datatype 2012 \
+                   --split_file dataset/KITTI2012_val.txt
